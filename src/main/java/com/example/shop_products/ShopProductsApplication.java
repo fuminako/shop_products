88a833bd2cd6d1ff0715999c2ef85f55da0f0842
Анастасия <fumi_nako@mail.ru>
2022-11-18 09:12:22 +0300

    Initial commit

---
+++ src/main/java/com/example/shop_products/ShopProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopProductsApplication.class, args);
+    }
+
+}
